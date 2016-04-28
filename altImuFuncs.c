ac3d0cc6d325cf59811882f17241c89b3e953d96
SeregaKai <pletnevsv@yandex.ru>
2016-04-28 09:17:19 +0300

    Добавил функции запроса показаний датчиков.

--- altImuFuncs.c
+++ altImuFuncs.c
@@ -61,3 +61,56 @@ void altIMU_initialize (void) {
 	
 	return 0; 
 } 
+
+// ВЫВОД ДАННЫХ
+
+// функция возвращает текущее значение угловой скорости (знаковое)
+int16_t getAngVel (void) {
+	
+	uint8_t d[2]; 
+
+	i2c_read_seq_byte(GYRO, GYRO_OUT_Z_L, 2, &d[0]); 
+			
+	return ((d[1] << 8) & 0xFF00) | (d[0] & 0x00FF); 
+}
+
+// функция возвращает проекуии вектора магнитного поля в системе координат датчика
+void getGaus (int16_t *x, int16_t *y, int16_t *z) {
+
+	#define A_NUM 6
+	uint8_t d[A_NUM] = {};
+	
+	i2c_read_seq_byte(COMPAS, COMPAS_OUT_X_H, A_NUM, &d[0]);
+	
+	*x = (d[0] | (d[1] << 8)); 
+	*y = (d[2] | (d[3] << 8)); 
+	*z = (d[4] | (d[5] << 8)); 
+}
+
+// функция возвращает проекции вектора силы тяжести в систем координат датчика
+void getAcceleration (uint16_t *x, uint16_t *y, uint16_t *z) {
+	
+	#define A_NUM 6 
+	uint8_t d[A_NUM] = {};
+	int16_t temp[A_NUM/2]; 
+	double a_axis[A_NUM/2]; 	
+	
+	i2c_read_seq_byte(ACCEL, ACCEL_OUT_X_L, A_NUM, &d[0]);
+		
+	// ПЕРЕСЧЕТ
+	uint8_t j = 0;  
+	for (uint8_t i = 0; i < A_NUM/2; i ++) {
+		temp [i] = (d[j] | (d[++j] << 8)); // ??? ++j
+		j++;  
+	}
+	
+	for (uint8_t i = 0; i < A_NUM/2; i ++) a_axis [i] = (double)temp[i];
+	
+	double x_temp = atan(a_axis[0]/sqrt(a_axis[1]*a_axis[1] + a_axis[2]*a_axis[2]));
+	double y_temp = atan(a_axis[1]/sqrt(a_axis[2]*a_axis[2] + a_axis[0]*a_axis[0]));
+	double z_temp = atan(a_axis[2]/sqrt(a_axis[1]*a_axis[1] + a_axis[0]*a_axis[0]));
+	
+	*x = (int16_t)(180 * x_temp / 3.14);
+	*y = (int16_t)(180 * y_temp / 3.14);
+	*z = (int16_t)(180 * z_temp / 3.14);	
+}
--- altImuFuncs.h
+++ altImuFuncs.h
@@ -10,5 +10,9 @@ LPS331AP - pressure sensor
 
 void altIMU_initialize (void);
 
+int16_t getAngVel (void);
+void getGaus (int16_t *x, int16_t *y, int16_t *z);
+void getAcceleration (uint16_t *x, uint16_t *y, uint16_t *z);
+
 #endif //_ALTIMU_FUNCS_H
 
