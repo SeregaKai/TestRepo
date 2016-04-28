10614f2ab6af80cbac496bab1351affe26aa1380
SeregaKai <pletnevsv@yandex.ru>
2016-04-28 08:39:44 +0300

    Сброс ошибок глобальная функция.

--- altImuI2C.h
+++ altImuI2C.h
@@ -1,9 +1,11 @@
-ï»¿#ifndef ALT_IMU_I2C_H_
-#define ALT_IMU_I2C_H_
+#ifndef ALT_IMU_IC_H
+#define ALT_IMU_IC_H
 
-#include <asf.h>
+//#include <asf.h>
 
-uint16_t i2c_write_seq_byte(uint8_t deviceAddr, uint8_t regStartAddr, uint8_t num, uint8_t *d); 
+uint16_t i2c_write_seq_byte(uint8_t deviceAddr, uint8_t regStartAddr, uint8_t num, uint8_t *d);
 uint16_t i2ñ_read_seq_byte (uint8_t deviceAddr, uint8_t regStartAddr, uint8_t num, uint8_t *d);
+void i2c_reset_errors (void); 
 
-#endif /* ALT_IMU_I2C_H_ */
+
+#endif // ALT_IMU_IC_H
